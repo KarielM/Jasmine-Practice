5a4443958260cbe69cc57f1c6aea0465b391711a
KarielM <mosley.kariel@gmail.com>
2024-02-11 14:59:41 -0600

    practicing writing tests

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-//defining a User class
-class User {
-    name = ""
-    isActive = false;
-    
-    //method to update user's name
-    updateName(newName) {
-      this.name = newName; // sets the name property to the new name
+function KeyCodeDoorLock(isLocked, isHardLocked, password){
+    this.isArmed = true;
+    this.isHardLocked = false;
+    this.password = "password1234";
+    this.securityQuestionAnswer = "ice cream";
+    this.failures = 0;
+};
+
+KeyCodeDoorLock.prototype.armAlarm = function(enterPassword) {
+    if (enterPassword.length == 3){
+        this.isHardLocked = (enterPassword[0] != this.password 
+            && enterPassword[1] != this.password 
+            && enterPassword[2] != this.password ) ? true : false;
     }
-  
-    // method to toggle the user's active status
-    toggleStatus() {
-      this.isActive = !this.isActive; // toggle the value of the isActive property
+    else {
+        this.isArmed = (this.password = enterPassword) ? !this.isArmed : this.isArmed;
     }
 }
+
+KeyCodeDoorLock.prototype.changePassword = function(newPassword) {
+    this.password = (!this.isArmed) ? newPassword : this.password;
+};
+
+KeyCodeDoorLock.prototype.resetPassword = function(securityQuestion, newPassword) {
+        this.isHardLocked = (securityQuestion == this.securityQuestionAnswer) ? !this.isHardLocked : this.isHardLocked;
+        this.password = (securityQuestion == this.securityQuestionAnswer) ? newPassword : this.password;
+};
--- tests.js
+++ tests.js
@@ -1,21 +1,91 @@
-describe("User class", () => {
-  let user;
+describe("KeyCodeDoorLock", () => {
+  describe("initial state", () => {
+    const keyCodeDoorLock = new KeyCodeDoorLock();
 
-  beforeEach(() => {
-    // Create a new instance of User before each test
-    user = new User();
-    user.name = 'John Doe';
-    user.isActive = true;
+    it("starts disarmed", () => {
+      expect(keyCodeDoorLock.isArmed).toBe(true);
+    });
+
+    it("starts with password configured to 1234", () => {
+      expect(keyCodeDoorLock.password).toBe("password1234");
+    });
+
+    it("starts with hard lock disabled", () => {
+      expect(keyCodeDoorLock.isHardLocked).toBe(false);
+    });
+
+    it("starts with security question set to ice cream", () => {
+      expect(keyCodeDoorLock.securityQuestionAnswer).toBe("ice cream");
+    });
   });
 
-  it("should update the user's name", () => {
-    user.updateName('Jane Doe');
-    expect(user.name).toBe('Jane Doe');
+describe("arming and disarming alarm", () => {
+  it("can be armed and disarmed by entering the password", () => {
+    const keyCodeDoorLock = new KeyCodeDoorLock();
+    keyCodeDoorLock.isArmed = true;
+
+    keyCodeDoorLock.armAlarm("password1234");
+
+    expect(keyCodeDoorLock.isArmed).toBe(false);
   });
 
-  it("should toggle the user's active status", () => {
-    user.toggleStatus();
-    expect(user.isActive).toBe(false);
+  it("can be toggled with repeated entries of password", () => {
+    const keyCodeDoorLock = new KeyCodeDoorLock();
+    keyCodeDoorLock.armAlarm("password1234");
+    expect(keyCodeDoorLock.isArmed).toBe(false);
+
+    keyCodeDoorLock.armAlarm("password1234");
+    expect(keyCodeDoorLock.isArmed).toBe(true);
+
+    keyCodeDoorLock.armAlarm("password1234");
+    expect(keyCodeDoorLock.isArmed).toBe(false);
+
+    keyCodeDoorLock.armAlarm("password1234");
+    expect(keyCodeDoorLock.isArmed).toBe(true);
+  });
+  
+describe("changing password", () => {
+    it("changes the password", () => {
+      const keyCodeDoorLock = new KeyCodeDoorLock();
+      keyCodeDoorLock.isArmed = false;
+
+      keyCodeDoorLock.changePassword("5678");
+      expect(keyCodeDoorLock.password).toBe("5678");
+  
+      keyCodeDoorLock.changePassword("apple");
+      expect(keyCodeDoorLock.password).toBe("apple");
+    })
   });
 
+describe("hard locking", () => {
+  it("hard locks after entering incorrect password 3 times in a row", () => {
+    const keyCodeDoorLock = new KeyCodeDoorLock();
+
+    keyCodeDoorLock.armAlarm(["12345", "12345", "12345"]);
+    expect(keyCodeDoorLock.isHardLocked).toBe(true);
+  })
 });
+});
+describe("it allows resetting password while hard-locked", () => {
+  it('allows reset of password after verifying security question', () => {
+    const keyCodeDoorLock = new KeyCodeDoorLock();
+    keyCodeDoorLock.isHardLocked = true;
+
+
+
+    keyCodeDoorLock.resetPassword("ice cream", "45678");
+    expect(keyCodeDoorLock.isHardLocked).toBe(false);
+    expect(keyCodeDoorLock.password).toBe("45678");
+  })
+})
+});
+
+describe("password management", () => {
+  it("has to be disarmed to change password", () => {
+    const keyCodeDoorLock = new KeyCodeDoorLock();
+    keyCodeDoorLock.isArmed = true;
+    
+    keyCodeDoorLock.changePassword('5678');
+    expect(keyCodeDoorLock.password).toBe("password1234");
+  })
+})
